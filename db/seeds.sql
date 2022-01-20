d8284985969ae4f8e8b11970c35e34fc65c6a802
jclong <j.mail1014@gmail.com>
2022-01-19 21:03:30 -0500

    add database, tables and set

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO departments (`name`)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
+  ('Legal');
+
+INSERT INTO roles (title, salary)
+VALUES
+  ('Salesperson', 80000),
+  ('Lead Engineer', 150000),
+  ('Software Engineer', 120000),
+  ('Account Manager', 160000),
+  ('Accountant', 125000),
+  ('Legal Team Lead', 250000),
+  ('Lawyer', 190000);
+
+INSERT INTO employees (first_name, last_name)
+VALUES
+  ('Mike', 'Chan'),
+  ('Ashley', 'Rodriguez'),
+  ('Kevin', 'Tupik'),
+  ('Kunal', 'Singh'),
+  ('Malia', 'Brown'),
+  ('Sarah', 'Lourd'),
+  ('Tom', 'Allen');
